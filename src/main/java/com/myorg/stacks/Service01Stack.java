f3d38fcf26130d0032db72749d854db9b77810bb
vinicciusnev <xxnashflows@hotmail.com>
2025-09-12 21:50:22 -0300

    feat: Implementando classe responsavel por criar ALB Fargate

---
+++ src/main/java/com/myorg/stacks/Service01Stack.java
@@ -0,0 +1,66 @@
+package com.myorg.stacks;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+
+
+public class Service01Stack extends Stack {
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
+                .create(this, "ALB01")
+                .serviceName("service-01")
+                .cluster(cluster)
+                .cpu(512)
+                .memoryLimitMiB(1024)
+                .desiredCount(2)
+                .listenerPort(8080)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("spring-aws-microservice01")
+                                .image(ContainerImage.fromRegistry("vinicciusdev/spring-aws-microservice01:1.0.0"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
+                                                .logGroupName("Service01")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service01")
+                                        .build()))
+                                .build())
+                .publicLoadBalancer(true)
+                .build();
+
+        service01.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+                .path("/actuator/health")
+                .port("8080")
+                .healthyHttpCodes("200")
+                .build());
+
+        ScalableTaskCount scalableTaskCount = service01.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(2)
+                .maxCapacity(4)
+                .build());
+
+        scalableTaskCount.scaleOnCpuUtilization("Service01AutoScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(50)
+                .scaleInCooldown(Duration.seconds(60))
+                .scaleOutCooldown(Duration.seconds(60))
+                .build());
+
+    }
+}
